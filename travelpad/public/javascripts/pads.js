26c53a52d19cec5234b9de100681becf817333d7
TuckerDS <jjperez.tucker.dev@gmail.com>
2017-05-11 00:26:14 +0200

    file uplodad, basic crud, basic geo

---
+++ travelpad/public/javascripts/pads.js
@@ -0,0 +1,24 @@
+window.onload = eventLoad;
+function upload(){
+  document.getElementById("camera").click();
+ }
+
+function change() {
+  alert(document.getElementById("camera").value);
+  document.getElementById("filename").value = document.getElementById("camera").value ;
+}
+
+function geolocation (){
+  var position;
+  function onPositionReceived(position){
+    document.getElementById('lat').value = position.coords.latitude;
+    document.getElementById('lng').value = position.coords.longitude;
+  }
+  if (navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(onPositionReceived);
+  }
+}
+
+function eventLoad(){
+  geolocation();
+}
